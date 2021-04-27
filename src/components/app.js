4dbb08d4213e5d36b2ddc7ffa1c3d7292e689755
shubham kumar tiwari <shbhmtwr19@gmail.com>
2021-04-27 15:37:43 +0530

    first version

---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({ selected = false, onSelect = f => f }) => (  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />);
+
+const createArray = length => [...Array(length)];
+
+export default function StarRating({ totalStars = 5 }) {  const [selectedStars, setSelectedStars] = useState(0);  return (    <>      {createArray(totalStars).map((n, i) => (        <Star          key={i}          selected={selectedStars > i}          onSelect={() => setSelectedStars(i + 1)}        />      ))}      <p>        {selectedStars} of {totalStars} stars      </p>    </>  );}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StarRating from './components/app';
+
+  
+ReactDOM.render(<StarRating />, document.getElementById('root'));
